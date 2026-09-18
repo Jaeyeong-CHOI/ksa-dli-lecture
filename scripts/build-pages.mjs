ea862a112330d3a1a68aaeb94472fcfc67ee0c14
jaeyeong_openclaw <jaeyeong_openclaw@jaeyeong-openclawui-Macmini.local>
2026-09-18 15:23:31 +0900

    Integrate agentic coding practice into main navigation and search

--- scripts/build-pages.mjs
+++ scripts/build-pages.mjs
@@ -5,8 +5,8 @@ import {buildLectureFiles} from './build-lecture-files.mjs';
 console.log('Released lecture PDFs:',await buildLectureFiles());
 const html=readFileSync('dist/index.html','utf8');
 const routes=['agentic-coding','notebook-downloads','get-certification','notebooks','resources','practice',...lectures.map(l=>'lectures/'+l.slug),...notes.map(n=>'notes/'+n.slug)];
-for(const route of routes){mkdirSync('dist/'+route,{recursive:true});const page=route==='agentic-coding'?html.replace('</head>','<meta name="robots" content="noindex">\n</head>'):html;writeFileSync('dist/'+route+'/index.html',page)}
+for(const route of routes){mkdirSync('dist/'+route,{recursive:true});writeFileSync('dist/'+route+'/index.html',html)}
 writeFileSync('dist/404.html',html);writeFileSync('dist/.nojekyll','');
 writeFileSync('dist/CNAME','ksa.dli-lecture.com\n');
-writeFileSync('dist/sitemap.xml','<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'+['',...routes.filter(x=>!['practice','agentic-coding'].includes(x))].map(x=>'<url><loc>https://ksa.dli-lecture.com/'+x+(x?'/':'')+'</loc></url>').join('')+'</urlset>');
+writeFileSync('dist/sitemap.xml','<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'+['',...routes.filter(x=>x!=='practice')].map(x=>'<url><loc>https://ksa.dli-lecture.com/'+x+(x?'/':'')+'</loc></url>').join('')+'</urlset>');
 console.log('GitHub Pages routes:',routes.length);
